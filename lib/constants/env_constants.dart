a90cc46751cb34cdf17dd0544de7c8778d9dbc87
anaximeno <anaximenobrito@gmail.com>
2023-11-27 16:50:28 -0100

    feat: semi-implement api authentication
    Still a login form should be created to grant api tokens when users want to user the app, instead of using a coded token

---
+++ lib/constants/env_constants.dart
@@ -0,0 +1,4 @@
+abstract class EnvConstants {
+  // XXX: should completely implement authentication instead
+  static const apiToken = "43bee2d65436af4503e86c5fde3357682e5166a3";
+}
--- lib/infra/api.dart
+++ lib/infra/api.dart
@@ -1,10 +1,13 @@
+import 'package:ai4d_pests_app/constants/env_constants.dart';
 import 'package:dio/dio.dart' as dio;
 
 class Api {
-  // TODO: handle auth
   final String baseURL;
-
   final _dio = dio.Dio();
+
+  final defaultApiHeaders = {
+    'Authorization': 'Token ${EnvConstants.apiToken}',
+  };
 
   Api({this.baseURL = "http://127.0.0.1:8000/api"});
 
@@ -17,7 +20,7 @@ class Api {
       parseRoute(route),
       data: body,
       options: dio.Options(
-        headers: headers,
+        headers: {...defaultApiHeaders, ...?headers},
         responseType: dio.ResponseType.json,
       ),
     );
@@ -29,7 +32,7 @@ class Api {
     return _dio.get(
       parseRoute(route),
       queryParameters: params,
-      options: dio.Options(headers: headers),
+      options: dio.Options(headers: {...defaultApiHeaders, ...?headers}),
     );
   }
 
@@ -39,14 +42,14 @@ class Api {
     return _dio.put(
       parseRoute(route),
       data: body,
-      options: dio.Options(headers: headers),
+      options: dio.Options(headers: {...defaultApiHeaders, ...?headers}),
     );
   }
 
   Future<dio.Response> delete(String route, {Map<String, String>? headers}) {
     return _dio.delete(
       parseRoute(route),
-      options: dio.Options(headers: headers),
+      options: dio.Options(headers: {...defaultApiHeaders, ...?headers}),
     );
   }
 }
