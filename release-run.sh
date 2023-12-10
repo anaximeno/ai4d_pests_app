bc4a392c573abdf12a93de6cccee35a83f25e26e
anaximeno <anaximenobrito@gmail.com>
2023-12-10 16:14:17 -0100

    feat: add release run script

---
+++ release-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+TRY() {
+    COMMAND=$* ; $COMMAND ; ERR=$?
+    if [[ $ERR -ne 0 ]]
+    then
+        echo "Error ($ERR) while running '$COMMAND'"
+        exit $ERR
+    fi
+}
+
+
+HOSTNAME=0.0.0.0
+PORT=52148
+
+echo -e "*** Running AI4Pests Flutter Frontend in Release Mode ***\n"
+
+TRY flutter run --release -d web-server --web-hostname $HOSTNAME --web-port $PORT
